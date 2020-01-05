5a00c0c13ea2cb1d7f875410b0b1b8447bdf1779
brunoh1990 <bruno.henriques@devoteam.com>
2020-01-05 20:43:04 +0000

    changed stuff

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Olá!
+         Buenos Dias!
         </p>
         <a
           className="App-link"
